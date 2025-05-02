70d816793ed6223f860b8c525144e0701659bd5f
pailinglo <pailinglo@gmail.com>
2025-05-02 16:37:20 -0400

    Jupyter nb for image classifier

---
+++ GroceryDB/Clean GroceryDB for import.sql
@@ -0,0 +1,17 @@
+-- Reset GroceryDB --
+use GroceryDB
+-- clean all ingredients and User ingredients
+begin transaction
+
+Delete from RecipeIngredients
+Delete from RecipeCuisine
+Delete from RecipeDishType
+Delete from Recipes
+
+commit transaction
+
+
+--These are not needed, because when building the Ingredients table, all the recipes are considered. no filtering
+--In that sense, only recipe related tables need to be considered.
+--Delete from Ingredients
+--Delete from UserIngredients
